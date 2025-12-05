feeb217690dc7f44d3e8812cc015020f6dedf670
hamzabelkhouja <belkhouja.hamza@esprit.tn>
2025-12-05 06:37:45 +0100

    nchalh likhra9

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -5,10 +5,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")   // <<--- AJOUTE ÇA
+@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+        // Test qui vérifie que le contexte démarre correctement
+    }
+
+    @Test
+    void mainMethodRuns() {
+        // Appelle la méthode main pour couvrir 100% du fichier
+        StudentManagementApplication.main(new String[] {});
     }
 }
